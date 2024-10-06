631da0e134b5114ee80a4af8292c55f3b9257179
42JINAM <jinam@student.42seoul.kr>
2024-10-06 12:36:16 +0200

    Read file
    read file

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -1,13 +1,22 @@
 package simulator;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Simulator {
-  private void parsingScenario(String file) {
+  private void parsingScenario(String file) throws IOException {
     try {
-
-    } catch (Exception e) {
+      BufferedReader reader = new BufferedReader(
+        new FileReader(file)
+      );
+      String tmp;
+      if ((tmp = reader.readLine()) == null)
+        throw new IOException(" wrong scenario.txt");
+      while ((tmp = reader.readLine()) != null) {
+        System.out.println(tmp);
+      }
+    } catch (IOException e) {
       // TODO: handle exception
       throw new IOException(" Can not found file : " + file, e);
     }
@@ -15,15 +24,16 @@ public class Simulator {
   }
 
   public static void main(String[] argv) {
-    if (argv.length != 1) {
+    if (argv.length < 1) {
       System.out.println("Usage: java simulator.Simulator [scenario.txt]\n");
       return;
     }
     try {
       Simulator simulator = new Simulator();
-      simulator.parsingScenario(argv[1]);
+      simulator.parsingScenario(argv[0]);
+      // System.out.println(argv);
       return;
-    } catch (Exception e) {
+    } catch (IOException e) {
       // TODO: handle exception
       System.err.println("ERROR : " + e.getMessage());
     }
